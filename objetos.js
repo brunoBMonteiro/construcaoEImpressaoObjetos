ace671dea9c2ba945ec890a2e4dcdf13a3cb4ad3
Bruno Bilheri <52968377+brunoBMonteiro@users.noreply.github.com>
2020-09-07 18:41:33 -0300

    construção e impressão de objetos

---
+++ objetos.js
@@ -0,0 +1,10 @@
+const empresa = {
+    name: 'Rocketseat',
+    color: 'Roxo',
+    focus: 'Programação',
+    address: {
+        street: 'Rua Guilherme Gembala',
+        number: 260
+    }
+};
+ console.log(`A empresa ${empresa.name} está localizada em ${empresa.address.street}, ${empresa.address.number}`)
